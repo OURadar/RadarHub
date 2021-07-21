bf9c44a107c473be9948ae34348d06ed12accf75
boonleng <boonleng@ou.edu>
2021-07-21 17:23:22 -0500

    Port forwarding using 6380

--- radarhub/settings.py
+++ radarhub/settings.py
@@ -135,7 +135,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('localhost', 6379)]
+            'hosts': [('localhost', 6380)]
         }
     }
 }
