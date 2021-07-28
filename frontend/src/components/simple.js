ad5db5c2318273f6275046b95a227f65ee1ca5fb
boonleng <boonleng@ou.edu>
2021-07-28 09:31:50 -0500

    Moved simple components to a separate collection

---
+++ frontend/src/components/simple.js
@@ -0,0 +1,33 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import { Refresh, AccountCircle } from "@material-ui/icons";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./theme";
+
+export function StatusBar(props) {
+  return (
+    <div className="statusBar">
+      <div className="leftPadded">{props.message}</div>
+    </div>
+  );
+}
+
+export function Pad(props) {
+  return (
+    <div id="upperRight">
+      <ThemeProvider theme={theme}>
+        <IconButton
+          aria-label="Refresh"
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          <Refresh style={{ color: "var(--system-background)" }} />
+        </IconButton>
+        <IconButton>
+          <AccountCircle style={{ color: "var(--system-background)" }} />
+        </IconButton>
+      </ThemeProvider>
+    </div>
+  );
+}
