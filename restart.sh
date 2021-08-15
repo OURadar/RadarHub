b1549c7abed2a4698bacce1183be664e02b3adaa
boonleng <boonleng@ou.edu>
2021-08-14 19:05:44 -0500

    Needed update update.sh for supervisord

--- restart.sh
+++ restart.sh
@@ -3,19 +3,9 @@
 echo "Updating settings ..."
 sudo supervisorctl reread
 sudo supervisorctl update
-sudo systemctl daemon-reload
 
-echo "Stopping dgen ..."
-sudo systemctl stop dgen
-echo "Stopping frontend ..."
-sudo supervisorctl stop asgi:*
-echo "Stopping backhaul ..."
-sudo systemctl stop backhaul
+echo "Stopping services ..."
+sudo supervisorctl stop all
 
-
-echo "Starting backhaul ..."
-sudo systemctl start backhaul
-echo "Starting frontend ..."
-sudo supervisorctl start asgi:*
-echo "Starting dgen ..."
-sudo systemctl start dgen
+echo "Starting services ..."
+sudo supervisorctl start all
