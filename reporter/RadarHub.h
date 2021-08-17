eef7a48d614ecb49f643d5b5e5c7a8fee8067a63
boonleng <boonleng@ou.edu>
2021-08-17 10:48:14 -0500

    Added RadarHub.h

---
+++ reporter/RadarHub.h
@@ -0,0 +1,16 @@
+#ifndef __RadarHub__
+#define __RadarHub__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "types.h"
+#include "common.h"
+#include "RKWebSocket.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
