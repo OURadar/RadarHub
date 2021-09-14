b8a43da289fa675ac9b92714cf8d992278aa433f
boonleng <boonleng@ou.edu>
2021-09-14 13:40:53 -0500

    Binary, struct, and json straight from django

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.archive_profile, name='archive'),
-    path('archive/', views.archive, name='archive'),
-    path('archive/<str:radar>/', views.archive_radar, name='archive'),
+    path('control/<str:radar>/', views.archive_radar, name='archive'),
+    path('data/header/<str:name>/', views.header, name='binary'),
+    path('data/binary/<str:name>/', views.binary, name='binary'),
     path('', views.archive, name='archive'),
     # path('', views.index, name='index'),
     path('<str:radar>/', views.radar, name='radar'),
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,7 @@
-import json;
+import json
+import struct
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
@@ -17,3 +19,20 @@ def archive(request):
 
 def archive_profile(request):
     return archive_radar(request, "demo", True)
+
+def binary(request, name):
+    print(f'views.binary() name={name}')
+    elev = 0.5
+    elev_bin = bytearray(struct.pack('f', elev));
+    payload = elev_bin + b'\x00\x01\x02\x00\x00\x00\xfd\xfe\xff'
+    if not isinstance(payload, bytes):
+        payload = bytes(payload);
+    response = HttpResponse(payload, content_type='application/octet-stream')
+    return response
+
+def header(requst, name):
+    print(f'views.header() name =${name}')
+    data = {'elev': 0.5, 'count': 2000}
+    payload = json.dumps(data)
+    response = HttpResponse(payload, content_type='application/json')
+    return response
