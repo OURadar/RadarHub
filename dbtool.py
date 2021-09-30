afba1621931ee330d1fa37a612d7ceff54543e36
boonleng <boonleng@ou.edu>
2021-09-30 13:39:29 -0500

    Revising model for tarfile direct access

--- dbtool.py
+++ dbtool.py
@@ -15,6 +15,7 @@ import re
 import glob
 import django
 import pprint
+import tarfile
 import argparse
 import textwrap
 
@@ -27,16 +28,19 @@ pp = pprint.PrettyPrinter(indent=1, depth=2, width=60, sort_dicts=False)
 
 from frontend.models import File
 
-def insert(filename):
+def insert(filename, archive=None, offset=0, size=0):
     (path, name) = os.path.split(filename)
     if File.objects.filter(name=name):
         print(f'File {name} exists')
         return
     s = re.search(r'(?<=-)20[0-9][0-9][012][0-9][0-3][0-9]-[012][0-9][0-5][0-9][0-5][0-9]', name).group(0)
     datestr = f'{s[0:4]}-{s[4:6]}-{s[6:8]} {s[9:11]}:{s[11:13]}:{s[13:15]}Z'
-    x = File(name=name, path=path, date=datestr)
-    print(f'{x.name} :: {x.path} :: {x.date}')
-    x.save()
+    if archive:
+        x = File(name=name, path=archive, offset=offset, size=size, date=datestr)
+    else:
+        x = File(name=name, path=path, date=datestr)
+    print(f'{x.name} :: {x.path} :: {x.date} :: {x.offset} :: {x.size}')
+    # x.save()
 
 def retrieve(name):
     x = File.objects.filter(name=name)
@@ -45,6 +49,23 @@ def retrieve(name):
     if len(x) > 1:
         print(f'There are more than one match. Choosing the first one.')
     return x[0]
+
+def proc_archive(archive):
+    print(f'Processing {archive} ...')
+    with tarfile.open(archive) as aid:
+        for info in aid.getmembers():
+            # print(f'{info.name} {info.offset} {info.size}')
+            insert(info.name, archive=archive, offset=info.offset, size=info.size)
+
+def folder(folder):
+    if os.path.exists('_original'):
+        print('Listing _original ...')
+    else:
+        basename = os.path.basename(folder)
+        print(f'basename = {basename}')
+        archive = os.path.join(folder, f'{basename}.tgz')
+        if os.path.exists(archive):
+            proc_archive(archive)
 
 def main():
     parser = argparse.ArgumentParser(prog='dbtool.py',
@@ -57,6 +78,7 @@ def main():
             dbtool.py -i /data/PX1000/2013/20130520
             dbtool.py -v
         '''))
+    parser.add_argument('-i', dest='insert', action='append', help='insert a folder')
     parser.add_argument('-v', dest='verbose', default=0, action='count',
         help='increases verbosity')
     args = parser.parse_args()
@@ -75,9 +97,16 @@ def main():
     #     insert(file)
     # pp.pprint(files[0])
 
-    x = retrieve('PX-20130520-191140-E2.6-Z.nc')
-    print(x.name)
-    print(x.getFullpath())
+    print(f'args.insert = {args.insert}')
+    for path in args.insert:
+        if os.path.isdir(path):
+            folder(path)
+        elif 'tgz' in path:
+            proc_archive(path)
+
+    # x = retrieve('PX-20130520-191140-E2.6-Z.nc')
+    # print(x.name)
+    # print(x.getFullpath())
 
 if __name__ == '__main__':
     main()
--- frontend/models.py
+++ frontend/models.py
@@ -1,4 +1,5 @@
 import os
+import tarfile
 import numpy as np
 from netCDF4 import Dataset
 from django.db import models
@@ -6,9 +7,11 @@ from django.db import models
 # Create your models here.
 class File(models.Model):
     name = models.CharField(max_length=32)
-    path = models.CharField(max_length=512)
-    date = models.DateTimeField('date')
-
+    path = models.CharField(max_length=256)
+    offset = models.PositiveIntegerField(0)
+    size = models.PositiveIntegerField(0)
+    date = models.DateTimeField()
+    
     def __repr__(self):
         return f'name = {self.name}   path = {self.getFullpath()}'
 
@@ -26,34 +29,50 @@ class File(models.Model):
             return path
         return None
 
+    def read(self, fid):
+        with Dataset('dummy', mode='r', memory=fid.read()) as nc:
+            type = nc.getncattr('TypeName')
+            elevations = np.array(nc.variables['Elevation'][:], dtype=np.float32)
+            azimuths = np.array(nc.variables['Azimuth'][:], dtype=np.float32)
+            gatewidth = np.array(nc.variables['GateWidth'][:], dtype=np.float32)
+            values = np.array(nc.variables[type][:], dtype=np.float32)
+            values[values < -90] = np.nan
+            longitude = nc.getncattr('Longitude')
+            latitude = nc.getncattr('Latitude')
+            sweepElevation = nc.getncattr('Elevation')
+            sweepTime = nc.getncattr('Time')
+            symbol = self.name.split('.')[-2].split('-')[-1]
+        return {
+            'symbol': symbol,
+            'longitude': longitude,
+            'latitude': latitude,
+            'sweepTime': sweepTime,
+            'sweepElevation': sweepElevation,
+            'gatewidth': gatewidth,
+            'elevations': elevations,
+            'azimuths': azimuths,
+            'values': values
+        }
+
     def getData(self):
-        fullpath = self.getFullpath()
-        if fullpath is None:
-            return None
-        with open(fullpath, 'rb') as fid:
-            with Dataset('dummy', mode='r', memory=fid.read()) as nc:
-                type = nc.getncattr('TypeName')
-                elevations = np.array(nc.variables['Elevation'][:], dtype=np.float32)
-                azimuths = np.array(nc.variables['Azimuth'][:], dtype=np.float32)
-                gatewidth = np.array(nc.variables['GateWidth'][:], dtype=np.float32)
-                values = np.array(nc.variables[type][:], dtype=np.float32)
-                values[values < -90] = np.nan
-                longitude = nc.getncattr('Longitude')
-                latitude = nc.getncattr('Latitude')
-                sweepElevation = nc.getncattr('Elevation')
-                sweepTime = nc.getncattr('Time')
-                symbol = self.name.split('.')[-2].split('-')[-1]
-            return {
-                'symbol': symbol,
-                'longitude': longitude,
-                'latitude': latitude,
-                'sweepTime': sweepTime,
-                'sweepElevation': sweepElevation,
-                'gatewidth': gatewidth,
-                'elevations': elevations,
-                'azimuths': azimuths,
-                'values': values
-            }
+        if '.tgz' in self.path:
+            print('handling .tgz ...')
+            # Open archive
+            with tarfile.open(self.path) as aid:
+                name = f'./{self.name}'
+                print(f'extracting {name} from {self.path}')
+                info = aid.getmember(name)
+                print(f'info {info.name} {info.offset}')
+                with aid.extractfile(name) as fid:
+                    return self.read(fid)
+
+        else:
+            fullpath = self.getFullpath()
+            if fullpath is None:
+                return None
+        
+            with open(fullpath, 'rb') as fid:
+                return self.read(fid)
 
 # models.File.objects.filter(date__year=2015)
 # models.File.objects.filter(date__lte='2018-01-01 00:00Z')
