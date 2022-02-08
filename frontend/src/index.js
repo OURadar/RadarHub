1452233f2e711635e19e484e33d146664586443d
boonleng <boonleng@ou.edu>
2022-02-08 15:43:01 -0600

    Okay try this

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app5";
+import App from "./components/app6";
 
 const text = document.getElementById("params").textContent;
 const params = JSON.parse(text);
