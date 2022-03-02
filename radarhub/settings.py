0c7b88f3ef3a22dfca680a800f807b7624746458
boonleng <boonleng@ou.edu>
2022-03-02 11:55:03 -0600

    Added a test setup

--- radarhub/settings.py
+++ radarhub/settings.py
@@ -88,7 +88,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'radarhub.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -125,15 +124,6 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
