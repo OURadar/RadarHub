f1d80c2b0dcc147fbb87b0576d84498b5c4b7d8b
boonleng <boonleng@ou.edu>
2022-03-10 08:51:00 -0600

    Logging exception from tar.open()

--- radarhub/settings.py
+++ radarhub/settings.py
@@ -19,6 +19,7 @@ from common import color_name_value
 
 # My additional parameters
 VERBOSE = 0
+SIMULATE = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
