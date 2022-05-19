62ac9ebd7a65665b5bed98932acb3ff9722017ec
boonleng <boonleng@ou.edu>
2022-05-19 17:04:35 -0500

    Checkpoing. More on zulip bot

--- mars.py
+++ mars.py
@@ -14,43 +14,44 @@ import os
 import sys
 import time
 import signal
+import threading
 import setproctitle
 
 import common
 import dbtool
 
+bot = None
 logger = common.get_logger()
-keep_running = True
 
 def signalHandler(sig, frame):
     # Print a return line for cosmetic
     print('\r')
     logger.info('SIGINT received, finishing up ...')
-    global keep_running
-    keep_running = False
+    bot.stop()
 
 class RadarHubBot():
     def __init__(self):
         self.messenger = common.get_messenger()
-
-        # channel = "boonleng@ou.edu"
-        # topic = "bot-test"
-        # self.messenger.post('started', channel, topic)
+        self.want_running = False
+        self.running = False
+        self.tid = None
 
     def run(self):
-        global keep_running
-        keep_running = True
+        self.want_running = True
+        self.running = True
         tic = 0
-        while keep_running:
+        channel = 'boonleng@ou.edu'
+        self.messenger.post('started', channel)
+        while self.want_running:
             self.messenger.call_on_each_message_nonblock(self.onmessage)
-            print(f'tic = {tic}')
             tic += 1
-            time.sleep(1)
-        channel = "boonleng@ou.edu"
-        topic = "bot-test"
-        self.messenger.post('ended', channel, topic)
+        self.running = False
+        self.messenger.post('ended', channel)
 
-
+    def stop(self):
+        self.want_running = False
+        channel = 'boonleng-bot@chat.arrc.ou.edu'
+        self.messenger.post('jump', channel)
 
     def onmessage(self, message):
         if self.messenger.is_my_message(message):
@@ -60,15 +61,22 @@ class RadarHubBot():
         if message['is_me_message']:
             return
 
-        content = message['content']
+        content = message['content'].lower()
         sender_email = message['sender_email']
 
         if 'latest' in content:
             response = dbtool.check_latest()
-        elif 'bye' in content.lower():
+        elif 'hello' in content:
+            response = 'Hi there'
+        elif 'bye' in content:
             response = 'See you later :peace_sign:'
+        else:
+            response = None
 
-        self.messenger.post(response, channel=sender_email)
+        if response:
+            self.messenger.post(response, channel=sender_email)
+
+#
 
 if __name__ == '__main__':
     setproctitle.setproctitle(os.path.basename(sys.argv[0]))
