a3601fc07fa39f510899618ffc25b384c264227c
boonleng <boonleng@ou.edu>
2022-07-06 23:07:18 -0500

    URL namespace

--- reception/urls.py
+++ reception/urls.py
@@ -1,19 +1,18 @@
-from django.urls import include, path
+from django.urls import path
 
 from . import views
 
 from allauth.urls import provider_urlpatterns
-from allauth.account import views as allauth_views
 
 urlpatterns = [
     path('signin/', views.signin, name='signin'),
-    path('signout/', allauth_views.logout, name='logout'),
-    path('social/signup/', views.signup, name='signup'),
-    path('privacy/', views.privacy, name='privacy')
+    path('signout/', views.signout, name='signout'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('social/signup/', views.conflict, name='conflict'),
 ]
 
 # path('login/', allauth_views.login, name='login'),
 # import allauth.socialaccount.urls
-urlpatterns += [path("social/", include("allauth.socialaccount.urls"))]
+# urlpatterns += [path("social/", include("allauth.socialaccount.urls"))]
 
 urlpatterns += provider_urlpatterns
--- reception/views.py
+++ reception/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from allauth.account import views as allauth_views
+
 # Create your views here.
 
 def signin(request):
     return render(request, 'reception/signin.html')
 
-def signup(request):
-    return render(request, 'reception/signup.html')
+def signout(request):
+    return allauth_views.logout(request)
+
+def conflict(request):
+    return render(request, 'reception/conflict.html')
 
 def privacy(request):
     lines = [
