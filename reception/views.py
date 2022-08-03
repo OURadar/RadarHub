4f64a8f2f433034d485333b7773c7255d5fd1d0a
boonleng <boonleng@ou.edu>
2022-08-03 16:54:53 -0500

    Logos in SVG

--- reception/views.py
+++ reception/views.py
@@ -1,18 +1,27 @@
+import glob
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 from allauth.account import views as allauth_views
 
+lines = []
+for file in glob.glob('frontend/static/css/*.css'):
+    with open(file) as fid:
+        lines = [*lines, *fid.readlines()]
+css_hash = hash('\n'.join(lines))
+css_hash = f'{css_hash:08x}'[-8:]
+
 # Create your views here.
 
 def signin(request):
-    return render(request, 'reception/signin.html')
+    return render(request, 'reception/signin.html', {'css': css_hash})
 
 def signout(request):
     return allauth_views.logout(request)
 
 def conflict(request):
-    return render(request, 'reception/conflict.html')
+    return render(request, 'reception/conflict.html', {'css': css_hash})
 
 def privacy(request):
     lines = [
