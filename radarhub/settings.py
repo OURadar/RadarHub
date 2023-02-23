003d26f08ec16b039a6a90dfbb36e200515aec72
boonleng <boonleng@ou.edu>
2023-02-23 13:43:59 -0600

    Could have VERBOSE < 2 on master

--- radarhub/settings.py
+++ radarhub/settings.py
@@ -19,7 +19,7 @@ from pathlib import Path
 from common import color_name_value
 
 # My additional parameters
-VERBOSE = 2
+VERBOSE = 1
 SIMULATE = False
 
 # SECURITY WARNING    SECURITY WARNING    SECURITY WARNING    SECURITY WARNING
