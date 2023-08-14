285af27fb49b495b85b717921b726dfc3c2daf82
boonleng <boonleng@ou.edu>
2023-08-13 20:45:17 -0500

    Added algos module

---
+++ frontend/algos.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def passthrough(x):
+    return x
+
+def unfold(v, va=11.6):
+    mask = np.isfinite(v)
+    u = np.unwrap(np.nan_to_num(v), period=2*va, axis=-1)
+    u[~mask] = np.nan
+    return u
